d8e69aaea3d1dce0a7ac3126fdd1f018d256cc3a
EDMOND-FERENC MUREȘAN <edmond.muresan@stud.ubbcluj.ro>
2023-12-17 23:49:17 +0200

    lab10

---
+++ ShopPage.xaml.cs
@@ -0,0 +1,59 @@
+namespace Muresan_Edmond_Ferenc_Lab7;
+
+using Microsoft.Maui.Devices.Sensors;
+using Muresan_Edmond_Ferenc_Lab7.Models;
+using Plugin.LocalNotification;
+
+public partial class ShopPage : ContentPage
+{
+    public ShopPage()
+    {
+        InitializeComponent();
+    }
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Shop shop)
+        {
+            try
+            {
+                await App.Database.SaveShopAsync(shop);
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while saving shop: {ex.Message}");
+                // Handle the exception accordingly (display an error message, log the exception, etc.)
+            }
+        }
+    }
+    async void OnShowMapButtonClicked(object sender, EventArgs e)
+        {
+        var shop = (Shop)BindingContext;
+        var address = shop.Adress;
+        var locations = await Geocoding.GetLocationsAsync(address);
+        var options = new MapLaunchOptions
+        {
+            Name = "Magazinul meu preferat"
+        };
+        var location = locations?.FirstOrDefault();
+        // var myLocation = await Geolocation.GetLocationAsync();
+        var myLocation = new Location(46.7731796289, 23.6213886738);
+        var distance = myLocation.CalculateDistance(location,
+DistanceUnits.Kilometers);
+        if (distance < 4)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+        await Map.OpenAsync(location, options);
+    }
+}
